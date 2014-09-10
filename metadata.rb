8aed6f8b73c4936e52122fc4fe98d4e683ce923a
Daniel Perez <tuvistavie@gmail.com>
2014-09-10 19:43:57 +0900

    Add Berskfile.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tuvistavie@gmail.com'
 license 'MIT'
 description 'Installs and configures TOMOYO Linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.4'
+version '0.1.5'
 
 recipe "tomoyo::default", "TOMOYO Linux Installation"
 
