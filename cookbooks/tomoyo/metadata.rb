f4d9340d2d6c94a07e73f9c329602e784dbf78a1
Daniel Perez <tuvistavie@gmail.com>
2014-09-10 19:35:51 +0900

    Fix dependencies. Bump version.

--- cookbooks/tomoyo/metadata.rb
+++ cookbooks/tomoyo/metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'tuvistavie@gmail.com'
 license 'MIT'
 description 'Installs and configures TOMOYO Linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.1.4'
 
 recipe "tomoyo::default", "TOMOYO Linux Installation"
 
-%w{ apt }.each do |dep|
+%w{ apt reboot }.each do |dep|
   depends dep
 end
 
