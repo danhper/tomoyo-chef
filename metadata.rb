ffe0810df3db22aa9a73b2c84e76164090c5b9ce
Daniel Perez <tuvistavie@gmail.com>
2014-09-10 19:19:06 +0900

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tuvistavie@gmail.com'
 license 'MIT'
 description 'Installs and configures TOMOYO Linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 recipe "tomoyo::default", "TOMOYO Linux Installation"
 
